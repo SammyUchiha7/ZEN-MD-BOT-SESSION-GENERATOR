7fdb8380bcf1a92ed4125844ebe81ca2cff91a06
mxgamecoder <OlamilekanDamilaraa@gmail.com>
2024-11-10 04:55:57 -0800

    Add files via upload

--- id.js
+++ id.js
@@ -1,11 +1,10 @@
-function makeId(length = 20) {
+function makeid(num = 4) {
   let result = "";
-  const characters = "olamilekan123456789";
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var characters9 = characters.length;
+  for (var i = 0; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters9));
   }
   return result;
 }
-
-module.exports = { makeId };
+module.exports = {makeid};

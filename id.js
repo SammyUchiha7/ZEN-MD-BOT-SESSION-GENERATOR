6bc0cfc0d02a8e65a5885da6366979bb047c1dfe
mxgamecoder <OlamilekanDamilaraa@gmail.com>
2024-11-16 00:50:57 +0100

    Update id.js

--- id.js
+++ id.js
@@ -5,5 +5,5 @@ function makeid(length) {
     for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return result;  // This will return a string like "a7Yb5G"
+    return result;  // This will return a string like "a8Yb5G"
 }
